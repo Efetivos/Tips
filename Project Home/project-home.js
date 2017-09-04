45e01f9016699f9bae0b4ab9454dba2fa0a4e290
victor <victor.efetivos@gmail.com>
2017-09-03 22:11:03 -0300

    Project Home | Working

--- Project Home/project-home.js
+++ Project Home/project-home.js
@@ -1,21 +1,24 @@
 
 var tlHoverweb = new TimelineMax ({paused:true})
-	.from('#gig-web', 1,{x: 400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
+	.from('#gig-web', 1.5,{x: -400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
 	.to('#gig-web',.1, {autoAlpha:0})
 
 
 var tlHovervid = new TimelineMax ({paused:true})
-	.from('#gig-vid', 1,{x: 400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
+	.from('#gig-vid', 1.5,{x: -400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
 	.to('#gig-vid',.1, {autoAlpha:0})
 
 
 var tlHovermid = new TimelineMax ({paused:true})
-	.from('#gig-mid', 1,{x: 400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
+	.from('#gig-mid', 1.5,{x: -400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
 	.to('#gig-mid',.1, {autoAlpha:0})
 
 var tlHoverdes = new TimelineMax ({paused:true})
-	.from('#gig-des', 1,{x: 400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
+	.from('#gig-des', 1.5,{x: -400, autoAlpha:0, ease: Power4.easeOut}).addPause().add('exit')
 	.to('#gig-des',.1, {autoAlpha:0})
+
+
+
 
 $('#opt-web').mouseenter(function (){
 	tlHoverweb.play(0);
@@ -54,3 +57,74 @@ $('#opt-des').mouseleave(function (){
 	tlHoverdes.play('exit');
 	
 })
+
+
+	var tlHoverPweb = new TimelineMax ({paused:true})
+	.to('.title-gig',.6,{autoAlpha:0})
+	.to('.ctnr-options',1.5,{autoAlpha:0, y:-100, ease: Power3.easeInOut},'-=.5')
+	.from('#photo-web', 1.5,{height: 0, ease: Power3.easeInOut},'-=1.5')
+	.from('#trg-web',1,{autoAlpha:0});
+
+$('#opt-web').click(function (){
+	tlHoverPweb.play()
+	
+})
+
+$('#trg-web').click(function (){
+	tlHoverPweb.reverse()
+	
+})
+
+
+	var tlHoverPvid = new TimelineMax ({paused:true})
+	.to('.title-gig',.6,{autoAlpha:0})
+	.to('.ctnr-options',1.5,{autoAlpha:0, y:-100, ease: Power3.easeInOut},'-=.5')
+	.from('#photo-vid', 1.5,{height: 0, ease: Power3.easeInOut},'-=1.5')
+	.from('#trg-vid',1,{autoAlpha:0});
+
+$('#opt-vid').click(function (){
+	tlHoverPvid.play()
+	
+})
+
+$('#trg-vid').click(function (){
+	tlHoverPvid.reverse()
+	
+})
+
+
+	var tlHoverPmid = new TimelineMax ({paused:true})
+	.to('.title-gig',.6,{autoAlpha:0})
+	.to('.ctnr-options',1.5,{autoAlpha:0, y:-100, ease: Power3.easeInOut},'-=.5')
+	.from('#photo-mid', 1.5,{height: 0, ease: Power3.easeInOut},'-=1.5')
+	.from('#trg-mid',1,{autoAlpha:0});
+
+$('#opt-mid').click(function (){
+	tlHoverPmid.play()
+	
+})
+
+$('#trg-mid').click(function (){
+	tlHoverPmid.reverse()
+	
+})
+
+
+	var tlHoverPdes = new TimelineMax ({paused:true})
+	.to('.title-gig',.6,{autoAlpha:0})
+	.to('.ctnr-options',1.5,{autoAlpha:0, y:-100, ease: Power3.easeInOut},'-=.5')
+	.from('#photo-des', 1.5,{height: 0, ease: Power3.easeInOut},'-=1.5')
+	.from('#trg-des',1,{autoAlpha:0});
+
+$('#opt-des').click(function (){
+	tlHoverPdes.play()
+	
+})
+
+$('#trg-des').click(function (){
+	tlHoverPdes.reverse()	
+	
+})
+
+
+
