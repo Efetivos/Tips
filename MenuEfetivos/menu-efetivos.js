72237b39e4e754a9bd1ea2374e8c760db7358c90
victor <victor.efetivos@gmail.com>
2017-09-07 12:41:53 -0300

    Menu Efet Desk  Done | Working

--- MenuEfetivos/menu-efetivos.js
+++ MenuEfetivos/menu-efetivos.js
@@ -23,8 +23,8 @@ else {
 	
 	var tlMenuEfetMob = new TimelineMax()
 		
-		.from('.ctn-menu',1.5,{width:0, ease: Power3.easeInOut})
-		.staggerFrom('.link, span',1,{autoAlpha:0, y:800, ease: Power2.easeOut},0.05,'-=1.3')
+		.from('.ctn-menu, .box-link',1.5,{width:0, ease: Power3.easeInOut})
+		.staggerFrom('.link, span',1,{autoAlpha:0, y:800, ease: Power2.easeOut},0.05,'-=0.6')
 		.staggerFrom('.menu-icons',1,{autoAlpha:0, y:100, ease: Power2.easeOut},0.1,'-=0.9')
 	
     .reverse();
