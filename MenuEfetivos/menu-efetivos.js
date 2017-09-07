f2cddd239afcde471a8a47ab0a4b6e3d266943da
victor <victor.efetivos@gmail.com>
2017-09-07 09:29:26 -0300

    Menu Efet Mobile  Done | Working

---
+++ MenuEfetivos/menu-efetivos.js
@@ -0,0 +1,24 @@
+if ($( window ).width() < 960) {
+
+
+	var tlMenuEfet = new TimelineMax()
+		
+		.from('.ctn-menu',1.5,{width:0, ease: Power3.easeInOut})
+		.staggerFrom('.link',1,{autoAlpha:0, x:-300, ease: Power2.easeOut},0.1,'-=1.3')
+		.staggerFrom('.menu-icons',1,{autoAlpha:0, y:100, ease: Power2.easeOut},0.1,'-=0.9')
+	
+    .reverse();
+;   
+
+$(".trg-menu").click(function() {
+  tlMenuEfet.reversed(!tlMenuEfet.reversed());
+});
+
+	
+	
+	
+	
+	
+	
+} //fecha mobile
+
