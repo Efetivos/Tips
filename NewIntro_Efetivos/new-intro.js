ba43d799be6bab26b28091407c5683b595a89679
victor <victor.efetivos@gmail.com>
2017-09-14 04:09:12 -0300

    Tls Intro | Working

---
+++ NewIntro_Efetivos/new-intro.js
@@ -0,0 +1,78 @@
+var tlPhotos = new TimelineMax({repeat:-1})
+
+	.from('#fader1', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power4.easeIn })
+	.from('#w1', 0.00001, {autoAlpha: 0})
+	.to('#fader1', 1.5, {scaleY: 0, transformOrigin:'top right', ease: Power4.easeOut })
+	.from('#t1', 1.5, {y:700, ease: Power3.easeOut },'-=1.5')
+	.to('#t1', 2, {delay:1.5, y:-700, ease: Power3.easeIn })
+
+	.from('#fader2', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power3.easeIn },'-=1.5')
+	.from('#w2', 0.00001, {autoAlpha: 0})
+	.to('#fader2', 1.5, {scaleY: 0, transformOrigin:'top right', ease: Power4.easeOut })
+	.from('#t2', 1.5, {y:700, ease: Power3.easeOut },'-=1.5')
+	.to('#t2', 2, {delay:1.5, y:-700, ease: Power3.easeIn })
+
+	.from('#fader3', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power3.easeIn },'-=1.5')
+	.from('#w3', 0.00001, {autoAlpha: 0})
+	.to('#fader3', 1.5, {scaleY: 0, transformOrigin:'top right', ease: Power4.easeOut })
+	.from('#t3', 1.5, {y:700, ease: Power3.easeOut },'-=1.5')
+	.to('#t3', 2, {delay:1.5, y:-700, ease: Power3.easeIn })
+
+	.from('#fader4', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power3.easeIn },'-=1.5')
+	.from('#w4', 0.00001, {autoAlpha: 0})
+	.to('#fader4', 1.5, {scaleY: 0, transformOrigin:'top right', ease: Power4.easeOut })
+	.from('#t4', 1.5, {y:700, ease: Power3.easeOut },'-=1.5')
+	.to('#t4', 2, {delay:1.5, y:-700, ease: Power3.easeIn })
+
+	.from('#fader5', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power3.easeIn },'-=1.5')
+	.from('#w5', 0.00001, {autoAlpha: 0})
+	.to('#fader5', 1.5, {scaleY: 0, transformOrigin:'top right', ease: Power4.easeOut })
+	.from('#t5', 1.5, {y:700, ease: Power3.easeOut },'-=1.5')
+	.to('#t5', 2, {delay:1.5, y:-700, ease: Power3.easeIn })
+
+	.from('#fader6', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power3.easeIn },'-=1.5')
+	.from('#w6', 0.00001, {autoAlpha: 0})
+	.to('#fader6', 1.5, {scaleY: 0, transformOrigin:'top right', ease: Power4.easeOut })
+	.from('#t6', 1.5, {y:700, ease: Power3.easeOut },'-=1.5')
+	.to('#t6', 2, {delay:1.5, y:-700, ease: Power3.easeIn })
+
+	.from('#faderFinal', 1.5, {scaleY: 0, transformOrigin:'bottom right', ease: Power3.easeIn },'-=1.5');
+
+var tlMenu = new TimelineMax()
+	.staggerFrom('.menu-mob, .logo', 1, {y:-150, ease: Power1.easeOut},0.1)
+	.staggerFrom('.item-menu', 1, {y:-150, ease: Power1.easeOut},0.1)
+	.from('.text-projetos', 1,{y:200, ease: Power1.easeOut})
+	
+var serEfet = TweenMax.from('.text-ser-efet', 1,{y:-200, ease: Power1.easeOut})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			 
