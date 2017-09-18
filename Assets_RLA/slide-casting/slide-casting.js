aaac91e8eb1647ee6955055848569ea366526d65
victor <victor.efetivos@gmail.com>
2017-09-17 21:12:22 -0300

    casting-page

--- Assets_RLA/slide-casting/slide-casting.js
+++ Assets_RLA/slide-casting/slide-casting.js
@@ -17,20 +17,28 @@ var tlScroll= new TimelineMax({delay:1})
   	.to('.photo',2,{scale:.5,ease:Power4.easeOut},'-=2', )
   	.to(".img",2,{scale:1.2,ease:Power2.easeOut},'-=2')
 	.to('.photo', 1, {rotationX: 0, ease:Power1.easeOut},'-=1.7')
-	.to(window, 2, {scrollTo: '#ctn2', ease: Power4.easeInOut})
-	.to(".bg",.5, {css:{className:"bg2"}})
+	//===
+	.to(window, 2, {scrollTo: '#ctn2', ease: Power4.easeInOut}) //SCROLLTO
+	.to(".bg",.5, {css:{className:"bg2"}}) //CHANGE BG
+	//===
 	.to('.photo', 2, {rotationX: -10, ease:Power4.easeOut})
   	.to('.photo',2,{scale: 1,ease:Power4.easeOut},'-=2', )
   	.to(".img",2,{scale: 1,ease:Power2.easeOut},'-=2')
 	.to('.photo', 1, {rotationX: 0, ease:Power1.easeOut},'-=1.7')
 
+	
 
+
+
+	//=========== NEXT > PHOTO 3
 	.to('.photo', 2, {rotationX: 10, ease:Power4.easeOut})
   	.to('.photo',2,{scale:.5,ease:Power4.easeOut},'-=2', )
   	.to(".img",2,{scale:1.2,ease:Power2.easeOut},'-=2')
 	.to('.photo', 1, {rotationX: 0, ease:Power1.easeOut},'-=1.7')
-	.to(window, 2, {scrollTo: '#ctn3', ease: Power4.easeInOut})
-	.to(".bg", .5, {css:{className:"bg3"}})
+	//===
+	.to(window, 2, {scrollTo: '#ctn3', ease: Power4.easeInOut})//SCROLLTO  
+	.to(".bg", .5, {css:{className:"bg3"}})//CHANGE BG
+	//===
 	.to('.photo', 2, {rotationX: -10, ease:Power4.easeOut})
   	.to('.photo',2,{scale: 1,ease:Power4.easeOut},'-=2', )
   	.to(".img",2,{scale: 1,ease:Power2.easeOut},'-=2')
