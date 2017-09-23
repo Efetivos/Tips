04e058430e848586fe424ee9218988919c206428
victor <victor.efetivos@gmail.com>
2017-09-23 14:59:44 -0300

    Intro Efetivos 2018 | Desk done

--- IntroEfetivos_2018/intro-efetivos-2018.js
+++ IntroEfetivos_2018/intro-efetivos-2018.js
@@ -1,1 +1,22 @@
-// JavaScript Document
+var cursorDecor = $('#cursor-decor')
+
+$(document).on('mousemove', function(e){
+    $(cursorDecor).css({
+       left:  e.pageX -16,
+       top:   e.pageY - 16
+    });
+});
+
+
+$('#target, a').hover(
+  function() {
+    TweenMax.to(cursorDecor, .5,{scale:0.2});
+    TweenMax.to(this, .5,{scale:0.8});
+    TweenMax.to('#middle', .5,{scale:5});
+  } , function (){
+    TweenMax.to(cursorDecor, .5,{scale:1});
+     TweenMax.to(this, .5,{scale:1});
+    TweenMax.to('#middle', .5,{scale:1});
+  }
+);
+
