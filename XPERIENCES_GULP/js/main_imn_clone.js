ba4602ecc07e39528193c5a82210de65391c7064
victor <victor.efetivos@gmail.com>
2018-03-25 22:51:44 -0300

    Adjusts Decor Header | Done

--- XPERIENCES_GULP/js/main_imn_clone.js
+++ XPERIENCES_GULP/js/main_imn_clone.js
@@ -1,5 +1,8 @@
-TweenMax.set('.s1',{autoAlpha:0})
+TweenMax.set('.s2',{autoAlpha:0})
 
 if($(window).width() < 1024) {
     TweenMax.set($('.symbol-decor').not('.s1 .symbol-decor'),{autoAlpha:0})
 }
+
+// TweenMax.to('.box-photo',1.2,{delay:2, height:'0%', ease: Power4.easeInOut})
+// TweenMax.staggerTo('span',1,{opacity:0},1)
