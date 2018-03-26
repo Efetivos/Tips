9d8f47c9232066eb37e3b88592ebe8c3a25892da
victor <victor.efetivos@gmail.com>
2018-03-26 00:28:46 -0300

    Init Tl | Done

--- XPERIENCES_GULP/js/main_imn_clone.js
+++ XPERIENCES_GULP/js/main_imn_clone.js
@@ -4,8 +4,15 @@ if($(window).width() < 1024) {
     TweenMax.set($('.symbol-decor').not('.s1 .symbol-decor'),{autoAlpha:0})
 }
 
-// TweenMax.to('.box-photo',1.2,{delay:2, height:'0%', ease: Power4.easeInOut})
- TweenMax.staggerFrom('span',1,{x: 45},1)
 
- TweenMax.from('.line-triang', 1.6, {delay: 2, drawSVG:"0%", ease: Power2.easeOut})
- TweenMax.from('.symbol', 2, {delay: 2, opacity:0, yPercent: -20,  ease: Power3.easeOut})
+var tlSlideSection = new TimelineMax({delay:2, id:'InitTl'})
+    .staggerFrom('h1',.8,{cycle:{y:["-20", "20"]},opacity:0},.2)
+    .from('.line-triang', 1.6, { drawSVG:"0%", yPercent:-40,  ease: Power3.easeOut},.8)
+    .from('.part1', 2.2, { opacity:0, yPercent: 40,  ease: Power3.easeOut},.8)
+    .staggerFrom('.part4, .part2, .part3', 2, { opacity:0, yPercent: -20, fill:'white', ease: Power3.easeOut},.06,2.2)
+    .staggerFrom('.box-trigger',.8,{cycle:{x:["-10"]}, opacity:0},.2,3.1)
+    .from('.ctn-scroll-down',1.8,{yPercent:40, opacity:0},3)
+
+
+
+//GSDevTools.create({	minimal:false });
