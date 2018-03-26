a7ed20c70c405a30ebd8f6d2a4698206e3f81a00
victor <victor.efetivos@gmail.com>
2018-03-26 04:00:50 -0300

    Init Inidicator

--- XPERIENCES_GULP/js/main_imn_clone.js
+++ XPERIENCES_GULP/js/main_imn_clone.js
@@ -115,14 +115,26 @@ var time = .6,
 
         
  //       <!-- -------------------- TL INIT -------------------- -->
-/* var tlSlideSection = new TimelineMax({delay:2, id:'InitTl'})
-    .staggerFrom('h1',.8,{cycle:{y:["-20", "20"]},opacity:0},.2)
-    .from('.line-triang', 1.6, { drawSVG:"0%", yPercent:-40,  ease: Power3.easeOut},.8)
-    .from('.part1', 2.2, { opacity:0, yPercent: 40,  ease: Power3.easeOut},.8)
+var tlSlideSection = new TimelineMax({delay:2, id:'InitTl'})
+    .staggerFrom('.s1 h1',.8,{cycle:{y:["-20", "20"]},opacity:0},.2)
+    .from('.s1 .line-triang', 1.6, { drawSVG:"0%", yPercent:-40,  ease: Power3.easeOut},.8)
+    .from('.s1 .part1', 2.2, { opacity:0, yPercent: 40,  ease: Power3.easeOut},.8)
     .staggerFrom('.part4, .part2, .part3', 2, { opacity:0, yPercent: -20, fill:'white', ease: Power3.easeOut},.06,2.2)
     .staggerFrom('.box-trigger',.8,{cycle:{x:["-10"]}, opacity:0},.2,3.1)
     .from('.ctn-scroll-down',1.8,{yPercent:40, opacity:0},3)
-    .from('.trigger-menu, .imn-logo',1.8,{yPercent:-20, opacity:0},3) */
+    .from('.trigger-menu, .imn-logo',1.8,{yPercent:-20, opacity:0},3)
 
 
+
+
+
+//       <!-- -------------------- TL INIT -------------------- -->
+    $(window).bind('mousewheel DOMMouseScroll', function(event){
+        if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+            tlTrans.reverse()
+        }
+        else {
+            tlTrans.play()
+        }
+    });
 //GSDevTools.create({	minimal:false });
