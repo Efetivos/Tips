d1fefab831eb5239bda9881f70178f001fe9c968
victor <victor.efetivos@gmail.com>
2018-03-25 23:29:40 -0300

    Init Js

--- XPERIENCES_GULP/js/main_imn_clone.js
+++ XPERIENCES_GULP/js/main_imn_clone.js
@@ -1,4 +1,4 @@
-TweenMax.set('.s1',{autoAlpha:0})
+TweenMax.set('.s1, .s5, .s3, .s4',{autoAlpha:0})
 
 if($(window).width() < 1024) {
     TweenMax.set($('.symbol-decor').not('.s1 .symbol-decor'),{autoAlpha:0})
@@ -7,5 +7,5 @@ if($(window).width() < 1024) {
 // TweenMax.to('.box-photo',1.2,{delay:2, height:'0%', ease: Power4.easeInOut})
 // TweenMax.staggerTo('span',1,{opacity:0},1)
 
-TweenMax.from('.line-triang', 1.6, {delay: 2, drawSVG:"0%", ease: Power2.easeOut})
-TweenMax.from('.symbol', 2, {delay: 2, opacity:0, yPercent: -20,  ease: Power3.easeOut})
+ TweenMax.from('.line-triang', 1.6, {delay: 2, drawSVG:"0%", ease: Power2.easeOut})
+ TweenMax.from('.symbol', 2, {delay: 2, opacity:0, yPercent: -20,  ease: Power3.easeOut})
